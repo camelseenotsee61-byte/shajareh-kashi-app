859c53a9af4b2426cc80c21d75947141f56c9bb4
camelseenotsee61-byte <camelseenotsee61@gmail.com>
2026-09-25 05:37:52 +0330

    Update family_member.dart

--- lib/family_member.dart
+++ lib/family_member.dart
@@ -1,17 +1,23 @@
-// این مدل، الگوی هر فرد در شجره‌نامه است
 class FamilyMember {
+  final String id;
   final String name;
-  final String? birthDate; // علامت سوال یعنی این فیلد می‌تونه خالی هم باشه
+  final String? parentId; // شناسه سرشاخه یا والد
+  final String? parentName; // نام والد
+  final bool isAlive; // در قید حیات یا درگذشته
+  final String? birthDate;
   final String? deathDate;
   final String? spouse;
   final String? description;
 
-  FamilyMember({
+  const FamilyMember({
+    required this.id,
     required this.name,
+    this.parentId,
+    this.parentName,
+    this.isAlive = true,
     this.birthDate,
     this.deathDate,
     this.spouse,
     this.description,
   });
 }
-
