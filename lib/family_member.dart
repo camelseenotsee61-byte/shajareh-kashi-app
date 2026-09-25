7da177f795e73874a87743d5e0aa7aa3fe8d109e
camelseenotsee61-byte <camelseenotsee61@gmail.com>
2026-09-25 08:05:22 +0330

    Update family_member.dart

--- lib/family_member.dart
+++ lib/family_member.dart
@@ -1,23 +1,19 @@
 class FamilyMember {
-  final String? id;
   final String name;
-  final String? parentId;
-  final String? parentName;
-  final bool isAlive;
   final String? birthDate;
   final String? deathDate;
   final String? spouse;
   final String? description;
+  final bool isAlive;
+  final String? photoPath;
 
-  const FamilyMember({
-    this.id,
+  FamilyMember({
     required this.name,
-    this.parentId,
-    this.parentName,
-    this.isAlive = true,
     this.birthDate,
     this.deathDate,
     this.spouse,
     this.description,
+    this.isAlive = true,
+    this.photoPath,
   });
 }
